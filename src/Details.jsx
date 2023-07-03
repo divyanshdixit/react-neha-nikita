eb2347639417fcc9d8a52addbcacc2fb676e867c
Divyansh Dixir <divyanshdixit96@gmail.com>
2023-07-04 02:00:31 +0530

    component basic- props basic

---
+++ src/Details.jsx
@@ -0,0 +1,12 @@
+import HeadingComponent from "./Heading"
+
+const DetailComponent = () => {
+    return (
+        <div>
+            <HeadingComponent from="detail" />
+            <p> Para </p>
+        </div>
+    )
+}
+
+export default DetailComponent
---
+++ src/Heading.jsx
@@ -0,0 +1,11 @@
+
+// component => functional, class 
+// controlled, uncontrolled
+
+const HeadingComponent = (props) => {
+    return <h1> Divyansh - { props.from}  </h1>
+}
+
+// <h1>  </h1>
+// self closing <input/>
+export default HeadingComponent
