015ae23d865e5169f1e660c1a1c60272c4b71eec
Divyansh Dixir <divyanshdixit96@gmail.com>
2023-07-23 16:42:13 +0530

    to do delete and view

--- src/components/UseStateHook1.jsx
+++ src/components/UseStateHook1.jsx
@@ -1,27 +1,77 @@
-import React,{useState} from 'react'
+import React, { useEffect, useState } from "react";
 
 const UseStateHook1 = () => {
+  const [todoItems, setTodoItems] = useState({
+    title: "",
+    description: "",
+  });
+  const [items, setItems] = useState([]);
 
-   const [todoItems, setTodoItems] = useState({
-    title:'',
-    description:''
-   }); // [currentState, UpdateStateFunction]
-//    const [description, setDescription] = useState(''); // string
-
-   const handleChange = (event) => {
+  const handleChange = (event) => {
     console.log(event.target.value, event.target.name);
     // setTitle(event.target.value);
-    setTodoItems({
-        [event.target.name] : event.target.value
+    // setTodoItems({
+    //     [event.target.name] : event.target.value
+    // })
+
+    setTodoItems((prev) => {
+      console.log(prev);
+      return {
+        ...prev,
+        [event.target.name]: event.target.value,
+      };
+    });
+  };
+
+  const addItems = (e) => {
+    // items.push();
+    setItems((prev) => {
+      return [...prev, todoItems];
+    });
+  };
+
+  const handleDelete = (id) => {
+    const n1 = items.filter((i, idx) => {
+      return id !== idx
     })
-   }
+    setItems(n1);
+  };
 
   return (
     <div>
-      <input type='text' name='title' placeholder='add new title' value={todoItems.title} onChange={handleChange}/>
-      <input type='text' name='description' placeholder='add new description' value={todoItems.description} onChange={handleChange}/>
+      <input
+        type="text"
+        name="title"
+        placeholder="add new title1"
+        value={todoItems.title}
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="description"
+        placeholder="add new description1"
+        value={todoItems.description}
+        onChange={handleChange}
+      />
+      <button type="button" onClick={addItems}>
+        {" "}
+        Add{" "}
+      </button>
+      <div>
+        {" "}
+        {items.map((i, id) => {
+          return (
+            <>
+            <h1> {i.title} {i.description}  </h1>
+            <button onClick={() => handleDelete(id)}>
+             X
+            </button>
+            </>
+          )
+          })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default UseStateHook1
+export default UseStateHook1;
